31c19b512981d378b835a5be176844eae5fa449f
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-08-26 01:14:44 +0200

    fetch data from Ghibli API

---
+++ src/data/types.ts
@@ -0,0 +1,40 @@
+
+
+export type Film = {
+    id: string;
+	title: string;
+	image: string;
+    description: string;
+    director: string;
+    release_date: string;
+};
+
+export type UserFilm = Film & {
+    favorite: boolean;
+};
+
+export type FavoriteFilm = UserFilm & {
+    seen: boolean;
+    order: number;
+};
+
+// 	Film
+//  ↓
+// Ghibli API information
+
+// UserFilm
+//  ↓
+// Film + favorite status
+
+// FavoriteFilm
+//  ↓
+// UserFilm + information needed only
+// for the Favorites view
+
+
+export type ApiState =
+    | { status: "idle" }
+    | { status: "loading" }
+    | { status: "success"; data: Film[] }
+	| { status: "error"; message: string };
+
