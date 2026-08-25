337551e6363a206bcc74489bb18611c170e910bd
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-08-26 01:47:14 +0200

    create FilmCard component

---
+++ src/components/FilmCard.tsx
@@ -0,0 +1,30 @@
+import type { Film } from "../data/types.tsx";
+
+type FilmCardProps = {
+    film: Film;
+};
+
+const FilmCard = ({ film }: FilmCardProps) => {
+    return (
+        <article>
+            <h2>{film.title}</h2>
+
+            <img
+                src={film.image}
+                alt={film.title}
+            />
+
+            <p>{film.description}</p>
+
+            <p>
+                Director: {film.director}
+            </p>
+
+            <p>
+                Release year: {film.release_date}
+            </p>
+        </article>
+    );
+};
+
+export default FilmCard;
