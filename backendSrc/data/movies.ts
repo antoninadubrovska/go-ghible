5053adb74d18b3d5b15f67a83fde6e82a39ba710
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-09-13 14:10:03 +0200

    copy all 22 movies to backend

--- backendSrc/data/movies.ts
+++ backendSrc/data/movies.ts
@@ -1,35 +1,193 @@
 import type { Movie } from "../types.js";
 
-// TODO image links
+// // TODO image links
+// export const movies: Movie[] = [
+//   {
+//     id: 1,
+//     title: "Castle in the Sky",
+//     year: 1986,
+//     image: "https://image.tmdb.org/t/p/w500/npdY2AqfZ8J7T6sYwQ8J9w.jpg",
+//   },
+//   {
+//     id: 2,
+//     title: "My Neighbor Totoro",
+//     year: 1988,
+//     image: "https://image.tmdb.org/t/p/w500/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg",
+//   },
+//   {
+//     id: 3,
+//     title: "Kiki's Delivery Service",
+//     year: 1989,
+//     image: "https://image.tmdb.org/t/p/w500/7i3cG4Z5K5J4J6J7J8J9J0.jpg",
+//   },
+//   {
+//     id: 4,
+//     title: "Princess Mononoke",
+//     year: 1997,
+//     image: "https://image.tmdb.org/t/p/w500/pdtzEreKvKAlqa2YEBaGwiA45V.jpg",
+//   },
+//   {
+//     id: 5,
+//     title: "Spirited Away",
+//     year: 2001,
+//     image: "https://image.tmdb.org/t/p/w500/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+//   },
+// ];
+
+//TODO to change data type
 export const movies: Movie[] = [
-  {
-    id: 1,
-    title: "Castle in the Sky",
-    year: 1986,
-    image: "https://image.tmdb.org/t/p/w500/npdY2AqfZ8J7T6sYwQ8J9w.jpg",
-  },
-  {
-    id: 2,
-    title: "My Neighbor Totoro",
-    year: 1988,
-    image: "https://image.tmdb.org/t/p/w500/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg",
-  },
-  {
-    id: 3,
-    title: "Kiki's Delivery Service",
-    year: 1989,
-    image: "https://image.tmdb.org/t/p/w500/7i3cG4Z5K5J4J6J7J8J9J0.jpg",
-  },
-  {
-    id: 4,
-    title: "Princess Mononoke",
-    year: 1997,
-    image: "https://image.tmdb.org/t/p/w500/pdtzEreKvKAlqa2YEBaGwiA45V.jpg",
-  },
-  {
-    id: 5,
-    title: "Spirited Away",
-    year: 2001,
-    image: "https://image.tmdb.org/t/p/w500/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
-  },
-];
+	{
+	  id: 1,
+	  title: "Castle in the Sky",
+	  year: 1986,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg",
+	},
+	{
+	  id: 2,
+	  title: "Grave of the Fireflies",
+	  year: 1988,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/qG3RYlIVpTYclR9TYIsy8p7m7AT.jpg",
+	},
+	{
+	  id: 3,
+	  title: "My Neighbor Totoro",
+	  year: 1988,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg",
+	},
+	{
+	  id: 4,
+	  title: "Kiki's Delivery Service",
+	  year: 1989,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/7nO5DUMnGUuXrA4r2h6ESOKQRrx.jpg",
+	},
+	{
+	  id: 5,
+	  title: "Only Yesterday",
+	  year: 1991,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/xjJU6rwzLX7Jk8HFQfVW6H5guMC.jpg",
+	},
+	{
+	  id: 6,
+	  title: "Porco Rosso",
+	  year: 1992,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/byKAndF6KQSDpGxp1mTr23jPbYp.jpg",
+	},
+	{
+	  id: 7,
+	  title: "Pom Poko",
+	  year: 1994,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/kowo9E1e1JcWLXj9cCvAOFZcy5n.jpg",
+	},
+	{
+	  id: 8,
+	  title: "Whisper of the Heart",
+	  year: 1995,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/5E3Hvbu0bg38ouYf6chGftVGqZ7.jpg",
+	},
+	{
+	  id: 9,
+	  title: "Princess Mononoke",
+	  year: 1997,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/jHWmNr7m544fJ8eItsfNk8fs2Ed.jpg",
+	},
+	{
+	  id: 10,
+	  title: "My Neighbors the Yamadas",
+	  year: 1999,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/wTGuHmMIBBgKakY80J1D52VvQKI.jpg",
+	},
+	{
+	  id: 11,
+	  title: "Spirited Away",
+	  year: 2001,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+	},
+	{
+	  id: 12,
+	  title: "The Cat Returns",
+	  year: 2002,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/avPMO5cnaGHgLaNiAIhy33WoQLm.jpg",
+	},
+	{
+	  id: 13,
+	  title: "Howl's Moving Castle",
+	  year: 2004,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/TkTPELv4kC3u1lkloush8skOjE.jpg",
+	},
+	{
+	  id: 14,
+	  title: "Tales from Earthsea",
+	  year: 2006,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/67yYwCPq7NbxSF6BIIXCMD34sY0.jpg",
+	},
+	{
+	  id: 15,
+	  title: "Ponyo",
+	  year: 2008,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/mikKSEdk5kLhflWXbp4S5mmHsDo.jpg",
+	},
+	{
+	  id: 16,
+	  title: "Arrietty",
+	  year: 2010,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/oc2OB2KDmSRDMelKEAA1n4YRQL0.jpg",
+	},
+	{
+	  id: 17,
+	  title: "From Up on Poppy Hill",
+	  year: 2011,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/rRLYX4RZIyloHSJwvZKAhphAjiB.jpg",
+	},
+	{
+	  id: 18,
+	  title: "The Wind Rises",
+	  year: 2013,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/jfwSexzlIzaOgxP9A8bTA6t8YYb.jpg",
+	},
+	{
+	  id: 19,
+	  title: "The Tale of the Princess Kaguya",
+	  year: 2013,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/mWRQNlWXYYfd2z4FRm99MsgHgiA.jpg",
+	},
+	{
+	  id: 20,
+	  title: "When Marnie Was There",
+	  year: 2014,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/vug1dvDI1tSa60Z8qjCuUE7ntkO.jpg",
+	},
+	{
+	  id: 21,
+	  title: "The Red Turtle",
+	  year: 2016,
+	  image:
+		"https://image.tmdb.org/t/p/w600_and_h900_bestv2/wOBU3SLjQ9358Km9YWYasPZyebp.jpg",
+	},
+	{
+	  id: 22,
+	  title: "Earwig and the Witch",
+	  year: 2021,
+	  image:
+		"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/sJhFtY3eHuvvACaPpxpzdCLQqpQ.jpg",
+	},
+  ];
