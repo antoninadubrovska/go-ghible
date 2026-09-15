5de2764bb855b654989e9c2fa4398e8acba70ce9
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-09-15 14:03:34 +0200

    Add PUT review endpoint with validation

--- backendSrc/routes/reviews.ts
+++ backendSrc/routes/reviews.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 
 import { reviews } from "../data/reviews.js";
 import { movies } from "../data/movies.js";
-import { ReviewSchema } from "../types.js";
+//import { ReviewSchema } from "../types.js";
 import {
 	ReviewWithoutIdSchema,
 	type ReviewWithoutId,
@@ -71,6 +71,58 @@ router.post("/", (req, res): void => {
 	});
 
 	res.sendStatus(201);
+});
+
+// PUT /api/reviews/:id
+router.put("/:id", (req, res): void => {
+	const idString: string = req.params.id;
+	const id: number = Number(idString);
+
+	if (Number.isNaN(id)) {
+		res.status(400).send({
+			message: "Felaktigt id",
+		});
+		return;
+	}
+
+	let review: ReviewWithoutId;
+
+	try {
+		review = z.parse(ReviewWithoutIdSchema.strict(), req.body);
+	} catch (error) {
+		res.status(400).send({
+			message: "Felaktigt review-objekt",
+		});
+		return;
+	}
+
+	const movieExists: boolean = movies.some(
+		(movie) => movie.id === review.movieId,
+	);
+
+	if (!movieExists) {
+		res.status(400).send({
+			message: "Det finns ingen movie med detta id.",
+		});
+		return;
+	}
+
+	const foundIndex: number = reviews.findIndex(
+		(review) => review.id === id,
+	);
+
+	if (foundIndex >= 0) {
+		reviews[foundIndex] = {
+			...review,
+			id: id,
+		};
+
+		res.sendStatus(200);
+	} else {
+		res.status(404).send({
+			message: "Det finns ingen review med detta id.",
+		});
+	}
 });
 
 export default router;
