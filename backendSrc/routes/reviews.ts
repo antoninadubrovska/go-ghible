c3b5c437124c27e4d6560fa056636854b50578a1
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-09-15 13:05:13 +0200

    Add POST review endpoint with validation

--- backendSrc/routes/reviews.ts
+++ backendSrc/routes/reviews.ts
@@ -2,7 +2,12 @@ import { Router } from "express";
 import * as z from "zod";
 
 import { reviews } from "../data/reviews.js";
+import { movies } from "../data/movies.js";
 import { ReviewSchema } from "../types.js";
+import {
+	ReviewWithoutIdSchema,
+	type ReviewWithoutId,
+} from "../types.js";
 
 const router = Router();
 
@@ -29,6 +34,43 @@ router.get("/:id", (req, res): void => {
 	}
 
 	res.status(200).send(review);
+});
+
+// POST /api/reviews
+router.post("/", (req, res): void => {
+	let review: ReviewWithoutId;
+
+	try {
+		review = z.parse(ReviewWithoutIdSchema.strict(), req.body);
+	} catch (error) {
+		res.status(400).send({
+			message: "Felaktigt review-objekt",
+		});
+		return;
+	}
+
+	const movieExists: boolean = movies.some(
+		(movie) => movie.id === review.movieId,
+	);
+
+	if (!movieExists) {
+		res.status(400).send({
+			message: "Det finns ingen movie med detta id.",
+		});
+		return;
+	}
+
+	const newId: number =
+		reviews.length > 0
+			? Math.max(...reviews.map((review) => review.id)) + 1
+			: 1;
+
+	reviews.push({
+		...review,
+		id: newId,
+	});
+
+	res.sendStatus(201);
 });
 
 export default router;
